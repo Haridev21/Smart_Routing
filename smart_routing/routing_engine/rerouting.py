8c5e84508947e98d07b010a5b45ef8b78e6bd9cc
Haridev M <133084782+Haridev21@users.noreply.github.com>
2026-02-12 11:08:26 +0530

    Update rerouting.py

--- smart_routing/routing_engine/rerouting.py
+++ smart_routing/routing_engine/rerouting.py
@@ -10,7 +10,6 @@ def dynamic_reroute(car, cars_on_edges):
     start = car['current']  # current position
     end = car['end']
 
-    # Calculate new optimal path from current position
     new_path, cost = compute_shortest_path(start, end, cars_on_edges)
     car['current_path'] = new_path
     return car
